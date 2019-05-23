26d52fe8ddc2a7c5a3533c1b16c8401475d9a03b
becolomochi <becolomochi@gmail.com>
2019-05-23 23:16:45 +0900

    画像登録機能の追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_18_133138) do
+ActiveRecord::Schema.define(version: 2019_05_23_134450) do
 
   create_table "places", force: :cascade do |t|
     t.date "date"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2019_05_18_133138) do
     t.text "memo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "picture"
   end
 
 end
